b26ee798738365b6175396332c61d4128a4ac49a
Zachary <zbutler46@hotmail.com>
2023-07-24 21:05:31 -0400

    discounts and averages

--- average_numbers.py
+++ average_numbers.py
@@ -1,15 +1,20 @@
-numbers = []
+x = input("Enter a number: ")
+x = x.split()
+print(x)
 
-for n in numbers:
-    n = float(input("Enter several numbers separated by a comma to be averaged: "))
-    numbers.append(n)
-    print(numbers)
+numbers = []
+sum_num = sum(numbers)
+len_num = len(numbers)
+
+for x in numbers:
+    numbers.append(x)
+print(numbers)
 
 
 def average():
-    if len(numbers) != 0:
-        average = sum(numbers) / len(numbers)
-        print(f"The average of the numbers is: {average}")
+    # x = input("Enter several numbers separated by a comma to be averaged: ")
+    average = sum_num / len_num
+    print(f"The average of the numbers is: {average}")
 
 
 average()
---
+++ discount_table.py
@@ -0,0 +1,6 @@
+original_cost = [4.95, 9.95, 14.95, 19.95, 24.95]
+new_price = []
+
+for n in original_cost:
+    new_price = original_cost * 0.60
+    new_price.append()
