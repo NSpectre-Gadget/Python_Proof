01999eac68e4ec46b766fc983129a30289f9bfb8
Zachary <zbutler46@hotmail.com>
2023-09-07 15:47:29 -0400

    Identifying loss difference btw random variables

--- cross_entropy.py
+++ cross_entropy.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 
-# Loss Function
+# Loss Function - measuring the contrast between two random variables.
 def cross_entropy(target, pred):
     return -(target * np.log10(pred) + (1 - target) * (np.log10(1 - pred)))
